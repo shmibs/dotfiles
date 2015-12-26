c754d6e1a224d4ef295b7374cce6beb794cdc295
katherine <shmibs@shmibbles.me>
2015-12-26 13:01:04 -0700

    fix switcher
    maybe i'll actually bother using it now

--- .config/herbstluftwm/switcher.sh
+++ .config/herbstluftwm/switcher.sh
@@ -2,13 +2,13 @@
 
 source ~/.config/herbstluftwm/config_vars
 
-IFS=$'\r\n'  clients=($(wmctrl -l | cut -c -10))
-number=$(wmctrl -l | cut -c 20- | nl -w 2 -s ") " | \
+list=$(wmctrl -l)
+IFS=$'\r\n' nums=($(echo $list | cut -d ' ' -f 1))
+choice=$(echo $list | cut -d ' ' -f 5- | nl -w 2 -s ") " | \
 	dmenu -fn $efont -i -h $bheight -nb $bg_normal -nf $fg_normal \
 	-sb $bg_focus -sf $fg_focus -p "Select:" -l 40 | \
-	grep -oE '[0-9]' | head -1)
+	cut -d ')' -f 1)
 
-echo ${clients[$number]}
-if [ $number ]; then
-	herbstclient jumpto ${clients[$number]}
+if [ $choice ]; then
+	herbstclient jumpto ${nums[$choice]}
 fi
