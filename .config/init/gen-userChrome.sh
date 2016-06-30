2eee39add1b91b03acdbf4c8f9e6eed052a25013
katherine <shmibs@shmibbles.me>
2016-06-30 02:04:18 -0700

    fix indentation

--- .config/init/gen-userChrome.sh
+++ .config/init/gen-userChrome.sh
@@ -3,35 +3,35 @@ echo "\
 
 /* Get rid of the bottom border */
 #TabsToolbar:after {
-    border-bottom: 0px !important;
+	border-bottom: 0px !important;
 }
 
 /* Hide the new curved tab edges */
 .tab-background {
-    visibility: hidden !important;
+	visibility: hidden !important;
 }
 
 /* Hide the little graphic seperators between inactive tabs */
 .tabbrowser-tab:before,
 .tabbrowser-tab:after { 
-    visibility: hidden !important;
+	visibility: hidden !important;
 }
 
 #TabsToolbar {
-    -moz-appearance: none !important;
-    background: ${bg_normal} !important; 
+	-moz-appearance: none !important;
+	background: ${bg_normal} !important; 
 }
 
 .tabbrowser-tab {
-    background: ${bg_normal} !important;
-    color: ${fg_normal} !important;
+	background: ${bg_normal} !important;
+	color: ${fg_normal} !important;
 	font-family: \"${mfont}\" !important;
 	font-size: ${mfont_size}pt !important;
 }
 
 .tabbrowser-tab[selected] {
-    background: ${bg_focus} !important;
-    color: ${fg_focus} !important;
+	background: ${bg_focus} !important;
+	color: ${fg_focus} !important;
 	font-family: \"${mfont}\" !important;
 	font-size: ${mfont_size}pt !important;
 }
